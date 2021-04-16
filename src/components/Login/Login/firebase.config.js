bb51c665ff69f11d50924edf915b299c797deeef
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-04-16 09:54:55 +0600

    login page

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,21 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
+//     authDomain: "burj-al-arab.firebaseapp.com",
+//     databaseURL: "https://burj-al-arab.firebaseio.com",
+//     projectId: "burj-al-arab",
+//     storageBucket: "burj-al-arab.appspot.com",
+//     messagingSenderId: "244522202387",
+//     appId: "1:244522202387:web:f3801d0588387140e1d6e6"
+//   };
+//   export default firebaseConfig;
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5pAuiMXnx2dGSovfu5sGru-vLRUwRlh8",
+  authDomain: "doctors-portal-client-86d94.firebaseapp.com",
+  projectId: "doctors-portal-client-86d94",
+  storageBucket: "doctors-portal-client-86d94.appspot.com",
+  messagingSenderId: "3383267024",
+  appId: "1:3383267024:web:952deb6ea4850736d418d5"
+};
+export default firebaseConfig;
